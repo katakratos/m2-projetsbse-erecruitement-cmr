6c1218620af8afdaee769959ba4dc48e0a1fada2
Paule <85636937+katakratos@users.noreply.github.com>
2025-04-07 14:29:10 +0100

    ok

--- app/services/database.py
+++ app/services/database.py
@@ -1,10 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./erecruitement.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+#DATABASE_URL = "postgresql+psycopg2://postgres:paule123@localhost:5432/erecrutcmr"
+DATABASE_URL = (
+     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+ )
+engine = create_engine(DATABASE_URL) # connect_args={"check_same_thread": False}
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
