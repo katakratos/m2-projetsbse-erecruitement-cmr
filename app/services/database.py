ca8c47bf3ebbfbdf4ca99572fe4ea29aef796524
Paule <paulemeud@gmail.com>
2025-04-09 19:48:05 +0100

    update settings database

--- app/services/database.py
+++ app/services/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./erecruitement.db"
+from app.services.config import settings
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
